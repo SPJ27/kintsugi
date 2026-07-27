81ad79732a6c4d40a2a0f71560a84da7b134f6c1
Saksham Jain <saksham.khatod27@gmail.com>
2026-07-27 22:28:26 +0530

    feat: added db for better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,39 +2,37 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { genericOAuth } from "better-auth/plugins";
 import { db } from "./db";
+import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+  plugins: [
+    genericOAuth({
+      config: [
+        {
+          providerId: "hackclub",
+
+          discoveryUrl:
+            "https://auth.hackclub.com/.well-known/openid-configuration",
+
+          clientId: process.env.HACKCLUB_CLIENT_ID!,
+
+          clientSecret: process.env.HACKCLUB_CLIENT_SECRET!,
+
+          scopes: [
+            "openid",
+            "profile",
+            "email",
+            "verification_status",
+            "slack_id",
+          ],
+        },
+      ],
     }),
-
-    secret: process.env.BETTER_AUTH_SECRET!,
-
-    baseURL: process.env.BETTER_AUTH_URL!,
-
-    plugins: [
-        genericOAuth({
-            config: [
-                {
-                    providerId: "hackclub",
-
-                    discoveryUrl:
-                        "https://auth.hackclub.com/.well-known/openid-configuration",
-
-                    clientId: process.env.HACKCLUB_CLIENT_ID!,
-
-                    clientSecret:
-                        process.env.HACKCLUB_CLIENT_SECRET!,
-
-                    scopes: [
-                        "openid",
-                        "profile",
-                        "email",
-                        "verification_status",
-                        "slack_id",
-                    ],
-                },
-            ],
-        }),
-    ],
+    dash(),
+  ],
 });
