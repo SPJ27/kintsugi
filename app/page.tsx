3752b14791409c333b3a1bdbb7e03ee0f0bcad27
Saksham Jain <saksham.khatod27@gmail.com>
2026-08-08 14:42:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,54 @@
 "use client";
+
 import { authClient } from "@/lib/auth-client";
 
-const page = () => {
+const Page = () => {
   const { data: session, isPending, error } = authClient.useSession();
-  console.log(session?.user)
-  return (
-     <div>
-    {isPending && <h1>Loading...</h1>}
-     {session && <h1>{session.user.name}</h1>}
-     {!session && !isPending && <button
-    onClick={async () => {
-       await authClient.signIn.oauth2({
-             providerId: "hackclub",
-             callbackURL: "/auth/callback",
-          });
-        }}
-       >
-      Login
-     </button>}
-     {session && !isPending && <button
-       onClick={async () => {
-         await authClient.signOut()
-      }}
-     >
-       Logout
-      </button>}
-     </div>
-    <div>
-      
-    
 
+  console.log(session?.user);
+
+  return (
+    <div>
+      {isPending && <h1>Loading...</h1>}
+
+      {error && <p>Error loading session.</p>}
+
+      {session && <h1>{session.user.name}</h1>}
+
+      {!session && !isPending && (
+        <button
+          onClick={async () => {
+            await authClient.signIn.oauth2({
+              providerId: "hackclub",
+              callbackURL: "/auth/callback",
+            });
+          }}
+        >
+          Login
+        </button>
+      )}
+
+      {session && !isPending && (
+        <button
+          onClick={async () => {
+            await authClient.signOut();
+          }}
+        >
+          Logout
+        </button>
+      )}
+
+      {session && !session.user.hackatimeLinked && (
+        <a href="/api/hackatime/connect">
+          <button>Link Hackatime</button>
+        </a>
+      )}
+
+      {session && session.user.hackatimeLinked && (
+        <p>Hackatime Already Linked</p>
+      )}
+    </div>
   );
 };
 
-
-export default page;
+export default Page;
