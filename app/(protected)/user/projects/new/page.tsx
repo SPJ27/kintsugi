4ecc32232f029f31d4f2ce6ab417a51efb69855d
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-08-16 03:05:49 +0530

    feat : added a custom scrollbar to the landing page

--- app/(protected)/user/projects/new/page.tsx
+++ app/(protected)/user/projects/new/page.tsx
@@ -7,9 +7,9 @@ const kalam = Kalam({
 })
 export default function page(){
     return(
-      <main className={`${kalam.className} w-full flex flex-col justify-center items-center bg-[#2A1A08] px-12 py-5 rounded-3xl border-12 border-[#c9a030]`}>
-        <div className="text-2xl font-bold text-[#2A1A08]">
-            Create a Project            
+      <main className={`${kalam.className} w-full h-[89.09vh] overflow-y-auto flex flex-col items-center bg-[#2A1A08] px-12 py-5 rounded-3xl border-12 border-[#c9a030]`}>
+        <div className="text-4xl mx-auto font-bold">
+            Create a Project     
         </div>
         <p className="text-[#2A1A08]/60 text-xl font-medium">Add Your Project Details here</p>
         <div className="mt-4 flex flex-col items-center text-center">
